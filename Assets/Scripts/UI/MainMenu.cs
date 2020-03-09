2f121d04534712cf8c58f287c9572be1c9871679
Clement <clement@gabsee.com>
2020-03-09 13:36:51 +0100

    _ rework main menu effect on game start

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,6 +8,7 @@ using DG.Tweening;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private float m_startEffectDuration = 2.5f;
     [Header("Buttons")]
     [SerializeField] private Button m_start;
     [SerializeField] private Button m_twitterLink;
@@ -18,6 +19,7 @@ public class MainMenu : MonoBehaviour
     [Header("Texts")]
     [SerializeField] private CanvasGroup m_name;
     [SerializeField] private CanvasGroup m_subName;
+    [SerializeField] private CanvasGroup m_bottomInfos;
 
     [Header("Game UI")]
     [SerializeField] private List<SpriteRenderer> m_flowerPots;
@@ -60,23 +62,29 @@ public class MainMenu : MonoBehaviour
         m_loopSource.DOFade(0f, 3f);
 
         // Texts
-        m_name.transform.DOMoveY(m_name.gameObject.transform.position.y + 1f, 2.5f);
-        m_subName.transform.DOMoveY(m_subName.gameObject.transform.position.y + 1f, 2.5f);
+        m_name.transform.DOMoveY(m_name.gameObject.transform.position.y + 1f, m_startEffectDuration);
+        m_subName.transform.DOMoveY(m_subName.gameObject.transform.position.y + 1f, m_startEffectDuration);
 
-        m_name.DOFade(0f, 2.5f);
-        m_subName.DOFade(0f, 2f);
+        m_bottomInfos.transform.DOMoveY(m_bottomInfos.gameObject.transform.position.y - 0.5f, m_startEffectDuration);
+
+        m_name.DOFade(0f, m_startEffectDuration);
+
+        float textDuration = m_startEffectDuration - (m_startEffectDuration * 0.5f);
+        m_subName.DOFade(0f, textDuration);
+        m_bottomInfos.DOFade(0f, textDuration);
 
         // Buttons
-        m_start.transform.DOMoveY(m_start.gameObject.transform.position.y - 5f, 2.5f);
-        m_twitterLink.transform.DOMoveY(m_twitterLink.gameObject.transform.position.y - 5f, 2.5f);
-        m_kenneyLink.transform.DOMoveY(m_kenneyLink.gameObject.transform.position.y - 5f, 2.5f);
+        // m_start.transform.DOMoveY(m_start.gameObject.transform.position.y - 5f, 2.5f);
+        m_twitterLink.transform.DOMoveY(m_twitterLink.gameObject.transform.position.y - 5f, m_startEffectDuration);
+        m_kenneyLink.transform.DOMoveY(m_kenneyLink.gameObject.transform.position.y - 5f, m_startEffectDuration);
 
-        m_start.GetComponent<CanvasGroup>().DOFade(0f, 2.5f);
-        m_twitterLink.GetComponent<CanvasGroup>().DOFade(0f, 2.5f);
-        m_kenneyLink.GetComponent<CanvasGroup>().DOFade(0f, 2.5f);
+        m_start.GetComponent<CanvasGroup>().DOFade(0f, m_startEffectDuration - (m_startEffectDuration * 0.33f));
+        m_twitterLink.GetComponent<CanvasGroup>().DOFade(0f, m_startEffectDuration);
+        m_kenneyLink.GetComponent<CanvasGroup>().DOFade(0f, m_startEffectDuration);
 
         // Game UI
-        m_flowerPots.ForEach(s => s.DOFade(1f, 3f));
-        m_seedsPackages.ForEach(s => s.DOFade(1f, 3f));
+        float uiDuration = m_startEffectDuration + m_startEffectDuration * 0.5f;
+        m_flowerPots.ForEach(s => s.DOFade(1f, uiDuration));
+        m_seedsPackages.ForEach(s => s.DOFade(1f, uiDuration));
     }
 }
